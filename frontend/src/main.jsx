bb7d3b190bab05b139f0f8f48abfdf3d9dc9b570
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-09-18 23:02:25 +0530

    create router and route paths

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import Home from './Home.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './error-page.jsx';
+
+// Routes
+import Contact from './routes/Contact.jsx';
+import Login from './routes/Login.jsx';
+import Feedback from './routes/FeedBack.jsx';
+import FAQ from './routes/FAQ.jsx';
+import About from './routes/About.jsx';
+import Articles from './routes/Articles.jsx';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: '/',
+				element: <Articles />,
+			},
+			{
+				path: 'login',
+				element: <Login />,
+			},
+			{
+				path: 'feedback',
+				element: <Feedback />,
+			},
+			{
+				path: 'faq',
+				element: <FAQ />,
+			},
+			{
+				path: 'about',
+				element: <About />,
+			},
+			{
+				path: 'contact',
+				element: <Contact />,
+			},
+		],
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
