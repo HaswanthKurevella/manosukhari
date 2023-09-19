c120ccc3e7f8637eb88c9fe6d47bc032eb546a91
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-09-19 10:44:32 +0530

    add flex articles for testing

--- frontend/src/routes/Articles.jsx
+++ frontend/src/routes/Articles.jsx
@@ -1,5 +1,82 @@
+import './Articles.css';
+
 const Articles = () => {
-	return <>Arcticles</>;
+	return (
+		<>
+			<div className='articles'>
+				<div className='article'>
+					<div className='title'>Tittle</div>
+					<div className='description'>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi
+						ratione eos deserunt voluptas eius necessitatibus laborum deleniti
+						voluptatibus. Cumque odio labore atque minus iusto, voluptates
+						officiis. Totam, necessitatibus facilis?
+					</div>
+					<div className='author'>By Author</div>
+				</div>
+				<div className='article'>
+					<div className='title'>Tittle</div>
+					<div className='description'>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi
+						ratione eos deserunt voluptas eius necessitatibus laborum deleniti
+						voluptatibus. Cumque odio labore atque minus iusto, voluptates
+						officiis. Totam, necessitatibus facilis?
+					</div>
+					<div className='author'>By Author</div>
+				</div>
+				<div className='article'>
+					<div className='title'>Tittle</div>
+					<div className='description'>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi
+						ratione eos deserunt voluptas eius necessitatibus laborum deleniti
+						voluptatibus. Cumque odio labore atque minus iusto, voluptates
+						officiis. Totam, necessitatibus facilis?
+					</div>
+					<div className='author'>By Author</div>
+				</div>
+				<div className='article'>
+					<div className='title'>Tittle</div>
+					<div className='description'>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi
+						ratione eos deserunt voluptas eius necessitatibus laborum deleniti
+						voluptatibus. Cumque odio labore atque minus iusto, voluptates
+						officiis. Totam, necessitatibus facilis?
+					</div>
+					<div className='author'>By Author</div>
+				</div>
+				<div className='article'>
+					<div className='title'>Tittle</div>
+					<div className='description'>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi
+						ratione eos deserunt voluptas eius necessitatibus laborum deleniti
+						voluptatibus. Cumque odio labore atque minus iusto, voluptates
+						officiis. Totam, necessitatibus facilis?
+					</div>
+					<div className='author'>By Author</div>
+				</div>
+				<div className='article'>
+					<div className='title'>Tittle</div>
+					<div className='description'>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi
+						ratione eos deserunt voluptas eius necessitatibus laborum deleniti
+						voluptatibus. Cumque odio labore atque minus iusto, voluptates
+						officiis. Totam, necessitatibus facilis?
+					</div>
+					<div className='author'>By Author</div>
+				</div>
+				<div className='article'>
+					<div className='title'>Tittle</div>
+					<div className='description'>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi
+						ratione eos deserunt voluptas eius necessitatibus laborum deleniti
+						voluptatibus. Cumque odio labore atque minus iusto, voluptates
+						officiis. Totam, necessitatibus facilis?
+					</div>
+					<div className='author'>By Author</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Articles;
