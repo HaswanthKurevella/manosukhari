f1825d08ef498ae0f295e61da2715ddadf164edb
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-09-19 22:37:56 +0530

    add articles data to home page

--- frontend/src/routes/Articles.jsx
+++ frontend/src/routes/Articles.jsx
@@ -1,87 +1,43 @@
+import { useLoaderData } from 'react-router-dom';
 import './Articles.css';
 
-const API_KEY = import.meta.env.ARTICLES_API_KEY;
+const API_KEY = import.meta.env.VITE_ARTICLES_API_KEY;
 
 export async function loader() {
-	const data = await fetch(`https://newsapi.org/v2/everything?q=mentalhealth&apiKey=${API_KEY}`);
-	console.log(data);
+	const date = '2023-09-18';
+	const data = await fetch(
+		`https://newsapi.org/v2/everything?q=mental%20health&from=${date}&apiKey=${API_KEY}`
+	);
+	return data;
 }
 
 const Articles = () => {
+	const { articles } = useLoaderData();
+	let i = 0;
+	console.log(articles);
 	return (
 		<>
-			<div className='articles'>
-				<div className='article'>
-					<div className='title'>Tittle</div>
-					<div className='description'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi
-						ratione eos deserunt voluptas eius necessitatibus laborum deleniti
-						voluptatibus. Cumque odio labore atque minus iusto, voluptates
-						officiis. Totam, necessitatibus facilis?
-					</div>
-					<div className='author'>By Author</div>
+			{articles.length ? (
+				<div className='articles'>
+					{articles.map((article) => (
+						<div key={i++} className='article'>
+							<div className='article-title'>{article.title}</div>
+							<div className='description-container'>
+								<p className='article-description'>{article.description}</p>
+								{article.urlToImage && (
+									<img className='article-img' src={article.urlToImage} />
+								)}
+							</div>
+							<div className='meta-data'>
+								By <span className='article-author'>{article.author}</span> on{' '}
+								<span className='article-date'>{article.publishedAt}</span>
+							</div>
+						</div>
+					))}
 				</div>
-				<div className='article'>
-					<div className='title'>Tittle</div>
-					<div className='description'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi
-						ratione eos deserunt voluptas eius necessitatibus laborum deleniti
-						voluptatibus. Cumque odio labore atque minus iusto, voluptates
-						officiis. Totam, necessitatibus facilis?
-					</div>
-					<div className='author'>By Author</div>
-				</div>
-				<div className='article'>
-					<div className='title'>Tittle</div>
-					<div className='description'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi
-						ratione eos deserunt voluptas eius necessitatibus laborum deleniti
-						voluptatibus. Cumque odio labore atque minus iusto, voluptates
-						officiis. Totam, necessitatibus facilis?
-					</div>
-					<div className='author'>By Author</div>
-				</div>
-				<div className='article'>
-					<div className='title'>Tittle</div>
-					<div className='description'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi
-						ratione eos deserunt voluptas eius necessitatibus laborum deleniti
-						voluptatibus. Cumque odio labore atque minus iusto, voluptates
-						officiis. Totam, necessitatibus facilis?
-					</div>
-					<div className='author'>By Author</div>
-				</div>
-				<div className='article'>
-					<div className='title'>Tittle</div>
-					<div className='description'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi
-						ratione eos deserunt voluptas eius necessitatibus laborum deleniti
-						voluptatibus. Cumque odio labore atque minus iusto, voluptates
-						officiis. Totam, necessitatibus facilis?
-					</div>
-					<div className='author'>By Author</div>
-				</div>
-				<div className='article'>
-					<div className='title'>Tittle</div>
-					<div className='description'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi
-						ratione eos deserunt voluptas eius necessitatibus laborum deleniti
-						voluptatibus. Cumque odio labore atque minus iusto, voluptates
-						officiis. Totam, necessitatibus facilis?
-					</div>
-					<div className='author'>By Author</div>
-				</div>
-				<div className='article'>
-					<div className='title'>Tittle</div>
-					<div className='description'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi
-						ratione eos deserunt voluptas eius necessitatibus laborum deleniti
-						voluptatibus. Cumque odio labore atque minus iusto, voluptates
-						officiis. Totam, necessitatibus facilis?
-					</div>
-					<div className='author'>By Author</div>
-				</div>
-			</div>
+			) : (
+				<div className='title'>No articles availble</div>
+			)}
 		</>
 	);
 };
