2604079c4004be7dac7c01064abb280adbf38cb0
vamsimunagala <munagalavamsi37@gmail.com>
2023-09-22 15:34:02 +0530

    inital commit

--- frontend/src/routes/TherapistDirectory.jsx
+++ frontend/src/routes/TherapistDirectory.jsx
@@ -1,32 +1,9 @@
-import icon from "F:/sih/manosukhari/frontend/src/routes/icon.png";
-import "./TherapistDirectory.css";
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-
-
+const TherapistDirectory = () => {
   return (
-    <div className="doctor-list">
+    <div>
       <h1>Therapist Directory</h1>
-      <div className="card-container">
-        {therapists.map((therapist) => (
-          <div className="card" key={therapist._id}>
-            <img
-              style={{ width: "128px", height: "128px" }}
-              src={icon}
-              alt={`Profile pic of ${therapist.username}`}
-            />
-            <div className="desc">
-              <h1 style={{ whiteSpace: "nowrap" }}>{therapist.username}</h1>
-              <p>Email: {therapist.email}</p>
-              <p>Mobile: {therapist.mobile}</p>
-              <p>Qualification: {therapist.qualification}</p>
-              <button className="book">Book Appointment</button>
-            </div>
-          </div>
-        ))}
-      </div>
     </div>
   );
+};
 
 export default TherapistDirectory;
